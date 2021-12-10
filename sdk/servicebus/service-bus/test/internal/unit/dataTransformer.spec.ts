d5089d787cf85ee10c4d48bbc9b1ad01d46faa4f
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2021-12-09 23:08:46 -0800

    Remove assert imports

--- sdk/servicebus/service-bus/test/internal/unit/dataTransformer.spec.ts
+++ sdk/servicebus/service-bus/test/internal/unit/dataTransformer.spec.ts
@@ -4,11 +4,11 @@
 import { Buffer } from "buffer";
 import * as chai from "chai";
 const should = chai.should();
-import * as assert from "assert";
+import { assert } from "chai"
 import isBuffer from "is-buffer";
 import { defaultDataTransformer } from "../../../src/dataTransformer";
 
-describe("DataTransformer", function() {
+describe("DataTransformer", function () {
   const objectBody: any = {
     id: "123-456-789",
     weight: 10,
@@ -41,7 +41,7 @@ describe("DataTransformer", function() {
   const hexBufferBody: Buffer = Buffer.from("7468697320697320612074c3a97374", "hex");
   const transformer = defaultDataTransformer;
 
-  it("should correctly encode/decode a string message body", function(done) {
+  it("should correctly encode/decode a string message body", function (done) {
     const encoded: any = transformer.encode(stringBody, "data");
     encoded.typecode.should.equal(117);
     isBuffer(encoded.content).should.equal(true);
@@ -50,7 +50,7 @@ describe("DataTransformer", function() {
     done();
   });
 
-  it("should correctly encode/decode a number message body", function(done) {
+  it("should correctly encode/decode a number message body", function (done) {
     const encoded: any = transformer.encode(numberBody, "data");
     encoded.typecode.should.equal(117);
     isBuffer(encoded.content).should.equal(true);
@@ -59,7 +59,7 @@ describe("DataTransformer", function() {
     done();
   });
 
-  it("should correctly encode/decode a boolean message body", function(done) {
+  it("should correctly encode/decode a boolean message body", function (done) {
     const encoded: any = transformer.encode(booleanBody, "data");
     encoded.typecode.should.equal(117);
     isBuffer(encoded.content).should.equal(true);
@@ -68,7 +68,7 @@ describe("DataTransformer", function() {
     done();
   });
 
-  it("should correctly encode/decode a null message body", function(done) {
+  it("should correctly encode/decode a null message body", function (done) {
     const encoded: any = transformer.encode(nullBody, "data");
     encoded.typecode.should.equal(117);
     isBuffer(encoded.content).should.equal(true);
@@ -77,7 +77,7 @@ describe("DataTransformer", function() {
     done();
   });
 
-  it("should correctly encode/decode an undefined message body", function(done) {
+  it("should correctly encode/decode an undefined message body", function (done) {
     const encoded: any = transformer.encode(undefinedBody, "data");
     encoded.typecode.should.equal(117);
     isBuffer(encoded.content).should.equal(true);
@@ -86,7 +86,7 @@ describe("DataTransformer", function() {
     done();
   });
 
-  it("should correctly encode/decode an empty string message body", function(done) {
+  it("should correctly encode/decode an empty string message body", function (done) {
     const encoded: any = transformer.encode(emptyStringBody, "data");
     encoded.typecode.should.equal(117);
     isBuffer(encoded.content).should.equal(true);
@@ -95,7 +95,7 @@ describe("DataTransformer", function() {
     done();
   });
 
-  it("should correctly encode/decode an array message body", function(done) {
+  it("should correctly encode/decode an array message body", function (done) {
     const encoded: any = transformer.encode(arrayBody, "data");
     encoded.typecode.should.equal(117);
     isBuffer(encoded.content).should.equal(true);
@@ -104,7 +104,7 @@ describe("DataTransformer", function() {
     done();
   });
 
-  it("should correctly encode/decode an object message body", function(done) {
+  it("should correctly encode/decode an object message body", function (done) {
     const encoded: any = transformer.encode(objectBody, "data");
     encoded.typecode.should.equal(117);
     isBuffer(encoded.content).should.equal(true);
@@ -113,7 +113,7 @@ describe("DataTransformer", function() {
     done();
   });
 
-  it("should correctly encode/decode a buffer message body", function(done) {
+  it("should correctly encode/decode a buffer message body", function (done) {
     const encoded: any = transformer.encode(bufferBody, "data");
     encoded.typecode.should.equal(117);
     isBuffer(encoded.content).should.equal(true);
@@ -122,7 +122,7 @@ describe("DataTransformer", function() {
     done();
   });
 
-  it("should correctly encode/decode a hex buffer message body", function(done) {
+  it("should correctly encode/decode a hex buffer message body", function (done) {
     const encoded: any = transformer.encode(hexBufferBody, "data");
     encoded.typecode.should.equal(117);
     isBuffer(encoded.content).should.equal(true);
@@ -131,64 +131,64 @@ describe("DataTransformer", function() {
     done();
   });
 
-  describe("decode", function() {
+  describe("decode", function () {
     // It is possible that we receive an AMQP value type from the messages that were sent with
     // previously shipped version of the sdk. If so then we should be able to handle those scenarios.
-    it("should correctly decode a string message body", function(done) {
+    it("should correctly decode a string message body", function (done) {
       const decoded: any = transformer.decode(stringBody);
       decoded.should.equal(stringBody);
       done();
     });
 
-    it("should correctly decode a number message body", function(done) {
+    it("should correctly decode a number message body", function (done) {
       const decoded: any = transformer.decode(numberBody);
       decoded.should.equal(numberBody);
       done();
     });
 
-    it("should correctly decode a boolean message body", function(done) {
+    it("should correctly decode a boolean message body", function (done) {
       const decoded: any = transformer.decode(booleanBody);
       decoded.should.equal(booleanBody);
       done();
     });
 
-    it("should correctly decode a null message body", function(done) {
+    it("should correctly decode a null message body", function (done) {
       const decoded: any = transformer.decode(nullBody);
       should.equal(decoded, nullBody);
       done();
     });
 
-    it("should correctly decode an undefined message body", function(done) {
+    it("should correctly decode an undefined message body", function (done) {
       const decoded: any = transformer.decode(undefinedBody);
       should.equal(decoded, undefined);
       done();
     });
 
-    it("should correctly decode an empty string message body", function(done) {
+    it("should correctly decode an empty string message body", function (done) {
       const decoded: any = transformer.decode(emptyStringBody);
       decoded.should.equal(emptyStringBody);
       done();
     });
 
-    it("should correctly decode an array message body", function(done) {
+    it("should correctly decode an array message body", function (done) {
       const decoded: any = transformer.decode(arrayBody);
       assert.deepEqual(decoded, arrayBody);
       done();
     });
 
-    it("should correctly decode an object message body", function(done) {
+    it("should correctly decode an object message body", function (done) {
       const decoded: any = transformer.decode(objectBody);
       assert.deepEqual(decoded, objectBody);
       done();
     });
 
-    it("should correctly decode a buffer message body", function(done) {
+    it("should correctly decode a buffer message body", function (done) {
       const decoded: any = transformer.decode(bufferBody);
       assert.deepEqual(decoded, bufferBody);
       done();
     });
 
-    it("should correctly decode a hex buffer message body", function(done) {
+    it("should correctly decode a hex buffer message body", function (done) {
       const decoded: any = transformer.decode(hexBufferBody);
       assert.deepEqual(decoded, hexBufferBody);
       done();
